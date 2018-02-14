dc22c4c0e94993b5f79a028674f879122dce0f1f
Michael <dominuskelenth@gmail.com>
2018-02-13 20:53:10 -0700

    Test

--- app/src/main/java/net/wetfish/wetfish/ui/GalleryActivity.java
+++ app/src/main/java/net/wetfish/wetfish/ui/GalleryActivity.java
@@ -165,7 +165,7 @@ public class GalleryActivity extends AppCompatActivity implements
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        // as you specify a parent activity in AndroidManifest.xml
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
