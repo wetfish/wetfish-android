e0e648e82551c426ebdd93c674ff8d99214e4427
Michael <dominuskelenth@gmail.com>
2018-04-05 06:22:50 -0600

    Feature: Settings back button works across all supported devices
    Now when pressing the back button from the settings menu it will appropriately
    take the user back through nested settings pages, and on the main settings page,
    to the main screen.

--- app/src/main/java/net/wetfish/wetfish/ui/SettingsActivity.java
+++ app/src/main/java/net/wetfish/wetfish/ui/SettingsActivity.java
@@ -15,6 +15,7 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.RingtonePreference;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.text.TextUtils;
 import android.view.MenuItem;
@@ -160,6 +161,17 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                 || GeneralPreferenceFragment.class.getName().equals(fragmentName)
                 || DataSyncPreferenceFragment.class.getName().equals(fragmentName)
                 || NotificationPreferenceFragment.class.getName().equals(fragmentName);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     /**
